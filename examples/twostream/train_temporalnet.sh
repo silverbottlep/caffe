1511b4d8116977bbc957a8e0751784578627b098
Eunbyung Park <silverbottlep@gmail.com>
2015-05-06 18:46:01 -0700

    add model files

--- examples/twostream/train_temporalnet.sh
+++ examples/twostream/train_temporalnet.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env sh
 
-./build/tools/caffe train -gpu 0 \
-	--solver=examples/twostream/temporalnet_solver.prototxt
+#./build/tools/caffe train -gpu 0 \
+#	--solver=examples/twostream/temporalnet_solver.prototxt
 
-./build/tools/caffe train -gpu 0 \
-	--solver=examples/twostream/temporalnet_solver2.prototxt \
-	--snapshot=examples/twostream/snapshot/temporalnet_iter_50000.solverstate
+#./build/tools/caffe train -gpu 1 \
+#	--solver=examples/twostream/temporalnet_solver2.prototxt \
+#	--snapshot=examples/twostream/snapshot/temporalnet_iter_80000.solverstate
+
+#./build/tools/caffe train -gpu 1 \
+#	--solver=examples/twostream/temporalnet_solver3.prototxt \
+#	--snapshot=examples/twostream/snapshot/temporalnet_iter_120000.solverstate
 
 ./build/tools/caffe train -gpu 0 \
 	--solver=examples/twostream/temporalnet_solver3.prototxt \
-	--snapshot=examples/twostream/snapshot/temporalnet_iter_120000.solverstate
+	--snapshot=examples/twostream/snapshot/temporalnet_iter_80000.solverstate

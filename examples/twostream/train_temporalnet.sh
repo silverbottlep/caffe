b5f7fda761311acfd39d927b9594cbe047f571e1
Eunbyung Park <silverbottlep@gmail.com>
2015-05-07 20:37:03 -0700

    increase the limit of size of protobuf

--- examples/twostream/train_temporalnet.sh
+++ examples/twostream/train_temporalnet.sh
@@ -11,6 +11,6 @@
 #	--solver=examples/twostream/temporalnet_solver3.prototxt \
 #	--snapshot=examples/twostream/snapshot/temporalnet_iter_120000.solverstate
 
-./build/tools/caffe train -gpu 0 \
+./build/tools/caffe train -gpu 7 \
 	--solver=examples/twostream/temporalnet_solver3.prototxt \
 	--snapshot=examples/twostream/snapshot/temporalnet_iter_80000.solverstate
